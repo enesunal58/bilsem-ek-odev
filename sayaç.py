2a802010b6aa9cffbea2e9aa9edb777500460aeb
enesunal58 <92205650+enesunal58@users.noreply.github.com>
2022-03-26 07:24:21 +0300

    Update sayaç.py

--- sayaç.py
+++ sayaç.py
@@ -6,5 +6,22 @@ def countdown(time_sec):
         print(timeformat,end='\r')
         time.sleep(1)
         time_sec -= 1
-    print("Süre bitti!")
-countdown(5)
+    print("Ders bitti!")
+
+def countdowns(time_secs):
+    while time_secs:
+        mins, secs = divmod(time_secs, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat,end='\r')
+        time.sleep(1)
+        time_secs -= 1
+    print("Teneffüs bitti!")
+
+a = input("Ders için geri sayım istiyorsanız D teneffüs için istiyorsanız T tuşuna basınız: ")
+if a=="D":
+    countdown(2400)
+elif a=="T":
+    countdowns(300)
+else:
+    print("HATALI GİRİŞ!")
+    
